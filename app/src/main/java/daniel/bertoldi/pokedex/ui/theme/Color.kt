1ba6ffd7e52a48184d8ab2a7e904bc9c1abb534d
Daniel Bertoldi <dsdanielbertoldi@gmail.com>
2024-03-20 10:07:53 -0300

    :bento: Add more assets and refactor weight and height to enum

--- app/src/main/java/daniel/bertoldi/pokedex/ui/theme/Color.kt
+++ app/src/main/java/daniel/bertoldi/pokedex/ui/theme/Color.kt
@@ -16,24 +16,24 @@ val Teal200 = Color(0xFF03DAC5)
 
 /* Pokémon UI Data by Type */
 enum class PokemonUIData(val typeColor: Color, val backgroundColor: Color, val icon: Int) {
-    BUG(Color(0xFF8CB230), Color(0xFF8BD674), R.drawable.ic_grass_type),
-    DARK(Color(0xFF58575F), Color(0xFF6F6E78), R.drawable.ic_grass_type),
-    DRAGON(Color(0xFF0F6AC0), Color(0xFF7383B9), R.drawable.ic_grass_type),
-    ELECTRIC(Color(0xFFEED535), Color(0xFFF2CB55), R.drawable.ic_grass_type),
-    FAIRY(Color(0xFFED6EC7), Color(0xFFEBA8C3), R.drawable.ic_grass_type),
-    FIGHTING(Color(0xFFD04164), Color(0xFFEB4971), R.drawable.ic_grass_type),
-    FIRE(Color(0xFFFD7D24), Color(0xFFFFA756), R.drawable.ic_grass_type),
-    FLYING(Color(0xFF748FC9), Color(0xFF83A2E3), R.drawable.ic_grass_type),
-    GHOST(Color(0xFF556AAE), Color(0xFF8571BE), R.drawable.ic_grass_type),
+    BUG(Color(0xFF8CB230), Color(0xFF8BD674), R.drawable.ic_bug_type),
+    DARK(Color(0xFF58575F), Color(0xFF6F6E78), R.drawable.ic_dark_type),
+    DRAGON(Color(0xFF0F6AC0), Color(0xFF7383B9), R.drawable.ic_dragon_type),
+    ELECTRIC(Color(0xFFEED535), Color(0xFFF2CB55), R.drawable.ic_electric_type),
+    FAIRY(Color(0xFFED6EC7), Color(0xFFEBA8C3), R.drawable.ic_fairy_type),
+    FIGHTING(Color(0xFFD04164), Color(0xFFEB4971), R.drawable.ic_fighting_type),
+    FIRE(Color(0xFFFD7D24), Color(0xFFFFA756), R.drawable.ic_fire_type),
+    FLYING(Color(0xFF748FC9), Color(0xFF83A2E3), R.drawable.ic_flying_type),
+    GHOST(Color(0xFF556AAE), Color(0xFF8571BE), R.drawable.ic_ghost_type),
     GRASS(Color(0xFF62B957), Color(0xFF8BBE8A), R.drawable.ic_grass_type),
-    GROUND(Color(0xFFDD7748), Color(0xFFF78551), R.drawable.ic_grass_type),
-    ICE(Color(0xFF61CEC0), Color(0xFF91D8DF), R.drawable.ic_grass_type),
-    NORMAL(Color(0xFF9DA0AA), Color(0xFFB5B9C4), R.drawable.ic_grass_type),
+    GROUND(Color(0xFFDD7748), Color(0xFFF78551), R.drawable.ic_ground_type),
+    ICE(Color(0xFF61CEC0), Color(0xFF91D8DF), R.drawable.ic_ice_type),
+    NORMAL(Color(0xFF9DA0AA), Color(0xFFB5B9C4), R.drawable.ic_normal_type),
     POISON(Color(0xFFA552CC), Color(0xFF9F6E97), R.drawable.ic_poison_type),
-    PSYCHIC(Color(0xFFEA5D60), Color(0xFFFF6568), R.drawable.ic_grass_type),
-    ROCK(Color(0xFFBAAB82), Color(0xFFD4C294), R.drawable.ic_grass_type),
-    STEEL(Color(0xFF417D9A), Color(0xFF4C91B2), R.drawable.ic_grass_type),
-    WATER(Color(0xFF4A90DA), Color(0xFF58ABF6), R.drawable.ic_grass_type),
+    PSYCHIC(Color(0xFFEA5D60), Color(0xFFFF6568), R.drawable.ic_psychic_type),
+    ROCK(Color(0xFFBAAB82), Color(0xFFD4C294), R.drawable.ic_rock_type),
+    STEEL(Color(0xFF417D9A), Color(0xFF4C91B2), R.drawable.ic_steel_type),
+    WATER(Color(0xFF4A90DA), Color(0xFF58ABF6), R.drawable.ic_water_type),
 }
 
 /* Generic Background Colors */
@@ -116,11 +116,55 @@ val TextGrey = Color(0xFF747476)
 val TextNumber = Color(0x9917171B)
 
 /* Height Colors */
-val HeightShort = Color(0xFFFFC5E6)
-val HeightMedium = Color(0xFFAEBFD7)
-val HeightTall = Color(0xFFAAACB8)
+enum class HeightUIData(
+    val heightLimit: Int,
+    val color: Color,
+    val iconUnselected: Int,
+    val iconSelected: Int,
+) {
+    SHORT(
+        heightLimit = 10,
+        color = Color(0xFFFFC5E6),
+        iconUnselected = R.drawable.ic_height_short,
+        iconSelected = R.drawable.ic_height_short_selected,
+    ),
+    MEDIUM(
+        heightLimit = 100,
+        color = Color(0xFFAEBFD7),
+        iconUnselected = R.drawable.ic_height_medium,
+        iconSelected = R.drawable.ic_height_medium_selected,
+    ),
+    TALL(
+        heightLimit = 1000,
+        color = Color(0xFFAAACB8),
+        iconUnselected = R.drawable.ic_height_tall,
+        iconSelected = R.drawable.ic_height_tall_selected,
+    ),
+}
 
 /* Weight Colors */
-val WeightLight = Color(0xFF99CD7C)
-val WeightNormal = Color(0xFF57B2DC)
-val WeightHeavy = Color(0xFF5A92A5)
+enum class WeightUIData(
+    val weightLimit: Int,
+    val color: Color,
+    val iconUnselected: Int,
+    val iconSelected: Int,
+) {
+    LIGHT(
+        weightLimit = 10,
+        color = Color(0xFF99CD7C),
+        iconUnselected = R.drawable.ic_weight_light,
+        iconSelected = R.drawable.ic_weight_light_selected,
+    ),
+    NORMAL(
+        weightLimit = 100,
+        color = Color(0xFF57B2DC),
+        iconUnselected = R.drawable.ic_weight_normal,
+        iconSelected = R.drawable.ic_weight_normal_selected,
+    ),
+    HEAVY(
+        weightLimit = 1000,
+        color = Color(0xFF5A92A5),
+        iconUnselected = R.drawable.ic_weight_heavy,
+        iconSelected = R.drawable.ic_weight_heavy_selected,
+    ),
+}

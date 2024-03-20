5dfbdd5e744d1f9b4b875c5d0701b47745125945
Daniel Bertoldi <dsdanielbertoldi@gmail.com>
2024-03-20 10:09:36 -0300

    :wrench: Update AGP to 8.0.2
    Also enabled configuration cache for Gradle and changed nonFinalresIds to True

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,13 +5,13 @@ buildscript {
 
     dependencies {
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.45")
-        classpath("com.android.tools.build:gradle:8.0.0")
+        classpath("com.android.tools.build:gradle:8.0.2")
         classpath("org.jetbrains.kotlin:kotlin-serialization:1.8.10")
     }
 }// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.0.0" apply false
-    id("com.android.library") version "8.0.0" apply false
+    id("com.android.application") version "8.0.2" apply false
+    id("com.android.library") version "8.0.2" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
     id("io.gitlab.arturbosch.detekt") version "1.21.0" apply false
     id("com.google.dagger.hilt.android") version "2.45" apply false
